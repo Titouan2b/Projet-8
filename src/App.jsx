3f1e94ce20aab1a83a765f661f4b390d76bfe003
Titouan2b <titouan2bsur@gmail.com>
2023-10-16 02:52:16 +0200

    ajout d'image externe

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './styles/App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Error from './pages/Error/Error'
+import Logement from './pages/Logement/Logement'
 
 function App() {
 
@@ -12,6 +15,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/logement/:id" element={<Logement/>} />
+          <Route path="*" element={<Error/>} />
         </Routes>
       </BrowserRouter>
     </>
